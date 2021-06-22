6a604e659bc8d16689e3939afb72e3f10bfff1c8
igoreshirokov <igoreshirokov@yandex.ru>
2021-06-23 00:07:22 +0300

    add R favicon

--- components/Layouts/MainLayout.js
+++ components/Layouts/MainLayout.js
@@ -7,7 +7,7 @@ export const MainLayout = ({ children, title, admin }) => {
         <>
             <Head>
                 <title>{title}</title>
-                {/* <meta name="viewport" content="width=device-width, initial-scale=1" /> */}
+                <link rel="icon" type="image/svg+xml" href="favicon.svg" />
                 <meta name='viewport' content={`width=device-width,initial-scale=1${admin && ',maximum-scale=1.0'}`} />
                 <meta content='true' name='HandheldFriendly' />
                 <meta content='width' name='MobileOptimized' />

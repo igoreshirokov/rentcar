ec05dcc72f17a70b4995a4643671258c633dfa72
igoreshirokov <igoreshirokov@yandex.ru>
2021-07-01 04:17:25 +0300

    *

--- components/Layouts/MainLayout.js
+++ components/Layouts/MainLayout.js
@@ -13,7 +13,7 @@ export const MainLayout = ({ children, title, admin }) => {
             <Head>
                 <title>{title}</title>
                 <link rel="icon" type="image/svg+xml" href="favicon.svg" />
-                <meta name='viewport' content={`width=device-width,initial-scale=1.0,${isAdmin && 'maximum-scale=1.0'}`} />
+                <meta name='viewport' content={`width=device-width,initial-scale=1,maximum-scale=1.0`} />
                 <meta content='true' name='HandheldFriendly' />
                 <meta content='width' name='MobileOptimized' />
                 <meta content='yes' name='apple-mobile-web-app- capable' />
